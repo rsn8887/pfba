918b57ecc7da3d1c0412b1d7704ae173a9fbc3cd
cpasjuste <cpasjuste@gmail.com>
2016-09-02 16:38:39 +0200

    029738

---
+++ src/burn/snd/ics2115.h
@@ -0,0 +1,8 @@
+void ics2115_init(INT32 cpu_clock, void (*cpu_irq_cb)(INT32), UINT8 *sample_rom, INT32 sample_rom_size);
+void ics2115_reset();
+UINT8 ics2115read(UINT8 offset);
+void ics2115write(UINT8 offset, UINT8 data);
+void ics2115_adjust_timer(INT32 ticks);
+void ics2115_update(INT16 *outputs, int samples);
+void ics2115_exit();
+void ics2115_scan(INT32 nAction,INT32 * /*pnMin*/);
