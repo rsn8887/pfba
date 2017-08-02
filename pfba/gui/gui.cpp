c7b0c5aa7b75ac8c6e79794729ba8056805936d8
rsn8887 <rsn8887@users.noreply.github.com>
2017-08-02 14:57:23 -0500

    PFBA: fix flickering borders if FIT is disabled

--- pfba/gui/gui.cpp
+++ pfba/gui/gui.cpp
@@ -673,6 +673,10 @@ void Gui::Run() {
             } else if (key & Input::Key::KEY_FIRE1) {
                 if (romSelected != NULL
                     && romSelected->state != RomList::RomState::MISSING) {
+                    for (int i = 0; i < 3; i++) {
+                        Clear();
+                        Flip();
+                    }
                     RunRom(romSelected);
                 }
             } else if (key & Input::Key::KEY_MENU1) {
